9a51e2030825468329b5dcfe0b0aaa664442edd8
Osadhi <osadhiv@gmail.com>
2024-08-12 06:05:36 +0800

    fixed some bugs

--- components/editor.tsx
+++ components/editor.tsx
@@ -3,8 +3,10 @@
 import { useTheme } from "next-themes";
 import { useEdgeStore } from "@/lib/edgestore";
 import { BlockNoteEditor } from "@blocknote/core";
-import { BlockNoteView, useCreateBlockNote } from "@blocknote/react";
-import "@blocknote/react/style.css";
+import { useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+import "@blocknote/core/fonts/inter.css";
 
 interface EditorProps {
   onChange: (value: string) => void;
