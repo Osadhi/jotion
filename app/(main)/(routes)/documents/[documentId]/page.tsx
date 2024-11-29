2f679efe17b41acfd4d0ca292a636eb5c389cf98
Osadhi <28138658+Osadhi@users.noreply.github.com>
2024-11-29 13:32:50 +0800

    Fix Known Error

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -13,14 +13,16 @@ interface DocumentIdPageProps {
   params: { documentId: Id<"documents"> };
 }
 
-export default function DocumentIdPage({ params }: DocumentIdPageProps) {
+export default function DocumentIdPage({ params }: Readonly<DocumentIdPageProps>) {
   const Editor = useMemo(
-    () => dynamic(() => import("@/components/editor"), { ssr: false }),
-    [],
+      () => dynamic(() => import("@/components/editor"), { ssr: false }),
+      []
   );
+
   const document = useQuery(api.documents.getById, {
     documentId: params.documentId,
   });
+
   const update = useMutation(api.documents.update);
 
   const onChange = (content: string) => {
@@ -29,29 +31,31 @@ export default function DocumentIdPage({ params }: DocumentIdPageProps) {
 
   if (document === undefined) {
     return (
-      <div>
-        <Cover.Skeleton />
-        <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
-          <div className="space-y-4 pl-8 pt-4">
-            <Skeleton className="h-14 w-[50%]" />
-            <Skeleton className="h-4 w-[80%]" />
-            <Skeleton className="h-4 w-[40%]" />
-            <Skeleton className="h-4 w-[60%]" />
+        <div>
+          <Cover.Skeleton />
+          <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
+            <div className="space-y-4 pl-8 pt-4">
+              <Skeleton className="h-14 w-[50%]" />
+              <Skeleton className="h-4 w-[80%]" />
+              <Skeleton className="h-4 w-[40%]" />
+              <Skeleton className="h-4 w-[60%]" />
+            </div>
           </div>
         </div>
-      </div>
     );
   }
 
-  if (document === null) return <div>Not found</div>;
+  if (document === null) {
+    return <div>Not found</div>;
+  }
 
   return (
-    <div className="pb-40">
-      <Cover url={document.coverImage} />
-      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
-        <Toolbar initialData={document} />
-        <Editor onChange={onChange} initialContent={document.content} />
+      <div className="pb-40">
+        <Cover url={document.coverImage} />
+        <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+          <Toolbar initialData={document} />
+          <Editor onChange={onChange} initialContent={document.content} />
+        </div>
       </div>
-    </div>
   );
 }
